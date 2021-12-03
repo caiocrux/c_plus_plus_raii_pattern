d9df5ea583e0fa3e5dc2c891e35b8f46cf68a66c
Caio Cruz <caio.cruz@tpv-tech.com>
2021-12-03 13:28:34 -0400

    update with correct email

--- include/safe_thread.hpp
+++ include/safe_thread.hpp
@@ -1,7 +1,7 @@
 /*
  * safe_thread.hpp
  * 
- * Copyright 2021 Caio Felipe Cruz <caio.crux@gmai.com>
+ * Copyright 2021 Caio Felipe Cruz <caio.crux@gmail.com>
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
